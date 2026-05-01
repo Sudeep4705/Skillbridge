558f0058076688e1fa74dbcf481ba58d1a832706
Sudeep <sudeepsudee828@gmail.com>
2026-05-01 21:41:04 +0530

    handler issue

--- backend/Routes/auth.routes.js
+++ backend/Routes/auth.routes.js
@@ -19,6 +19,6 @@ router.post("/forgetPassword",AuthContoller.forgetpassController)
 
 router.post("/reset-password/:token",AuthContoller.resetPassController)
 
-router.get("/students")
+
 
 module.exports = router
